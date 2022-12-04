c5b42842bf649fbe73264b6db86fc91300e4229d
Nicholas Trilone <leadermasterguy2@gmail.com>
2022-12-04 15:01:21 -0500

    Fetch call error checking

--- src/app.js
+++ src/app.js
@@ -17,41 +17,48 @@ form.addEventListener('submit', async (event) => {
   const container = document.querySelector('.container');
   const template = document.querySelector('#template');
 
-  let i = 0;
-  response.results.map(post => {
-    i++;
-    if (i < 11) {
-      const newClone = template.content.cloneNode(true);
+  if (Array.isArray(response.results)) {
+    try {
+      let i = 0;
+      response.results.map(post => {
+        i++;
+        if (i < 11) {
+          const newClone = template.content.cloneNode(true);
 
-      const cloneImg = newClone.querySelector('.post__img')
-      const cloneUser = newClone.querySelector('.post__user');
-      const cloneDesc = newClone.querySelector('.post__desc');
+          const cloneImg = newClone.querySelector('.post__img')
+          const cloneUser = newClone.querySelector('.post__user');
+          const cloneDesc = newClone.querySelector('.post__desc');
 
-      cloneImg.src = post.urls.small;
-      cloneImg.alt = post.alt_description;
+          cloneImg.src = post.urls.small;
+          cloneImg.alt = post.alt_description;
 
-      const userFirst = post.user.first_name;
-      const userLast = post.user.last_name;
-      const userFull = `By: `
-      if (userFirst == null && userLast == null) {
-        cloneUser.innerText = userFull + 'Unknown Author';
-      } else if (userFirst != null) {
-        cloneUser.innerText = userFull + userFirst;
-      } if (userLast != null) {
-        cloneUser.innerText = userFull + userLast;
-      }
+          const userFirst = post.user.first_name;
+          const userLast = post.user.last_name;
+          const userFull = `By: `
+          if (userFirst == null && userLast == null) {
+            cloneUser.innerText = userFull + 'Unknown Author';
+          } else if (userFirst != null) {
+            cloneUser.innerText = userFull + userFirst;
+          } if (userLast != null) {
+            cloneUser.innerText = userFull + userLast;
+          }
 
-      if (post.description == null) {
-        cloneDesc.innerText = 'No Description';
-      }
-      else if (post.description.length < 100) {
-        cloneDesc.innerText = post.description;
-      } else {
-        cloneDesc.innerText = post.description.slice(0, 100) + '...';
-      }
+          if (post.description == null) {
+            cloneDesc.innerText = 'No Description';
+          }
+          else if (post.description.length < 100) {
+            cloneDesc.innerText = post.description;
+          } else {
+            cloneDesc.innerText = post.description.slice(0, 100) + '...';
+          }
 
-      container.appendChild(newClone);
+          container.appendChild(newClone);
+        }
+      })
     }
-  }).catch(err => console.log(err));
+    catch (error) {
+      container.innerText = 'Invalid Results!'
+    }
+  }
 }
 )
