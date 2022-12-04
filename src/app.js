cee442a89bea7ab041c2b43cd34fbab26e3dafcb
Nicholas Trilone <leadermasterguy2@gmail.com>
2022-12-04 14:34:00 -0500

    Description V1

--- src/app.js
+++ src/app.js
@@ -19,11 +19,8 @@ form.addEventListener('submit', async (event) => {
   const container = document.querySelector('.container');
   const template = document.querySelector('#template');
 
-  /*  Loop through the results[] array. For each result, create a clone of the
-     template and append it to the DOM element with the .container class. */
-
   let i = 0;
-  posts.forEach(post => {
+  response.results.forEach(post => {
     i++;
     if (i < 10) {
       const newClone = template.content.cloneNode(true);
@@ -32,34 +29,18 @@ form.addEventListener('submit', async (event) => {
       const cloneUser = newClone.querySelector('.post__user');
       const cloneDesc = newClone.querySelector('.post__desc');
 
-      /* Add an attribution statement below the image using the
-    postUser element and the photographer's name from dataObj */
-
-      const dataObj = response.results[0];
-      console.log(response.results[0]);
-
-
-
       cloneImg.src = dataObj.urls.small;
       cloneImg.alt = dataObj.alt_description;
-      cloneUser.innerText = post.user;
-      cloneDesc.innerText = 'AAAAAAAAAAAAAAAAAA';
+      cloneUser.innerText = `By: ${post.user.first_name} ${post.user.last_name}`;
+      if (post.description.length > 100) {
+        cloneDesc.innerText = post.description;
+      } else {
+        cloneDesc.innerText = post.description.slice(0, 100) + '...';
+      }
+
       container.appendChild(newClone);
 
     }
   }).catch(err => console.log(err));
 }
 )
-
-
-/*
-  Add an attribution statement below the image using the
-  postUser element and the photographer's name from dataObj
- */
-
-/*
-  Check the description of the post. If it's bot bull and less than 100 characters,
-  add the description from dataObj to the post. If it's more than 100 characters,
-  add the first 100 characters of the description from dataObj to the post followed by
-  an ellipsis (...)
-*/
