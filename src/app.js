564096d221fbf1eece00900f5a896bbda5401d68
Nicholas Trilone <leadermasterguy2@gmail.com>
2022-12-04 13:49:33 -0500

    console log statements

--- src/app.js
+++ src/app.js
@@ -17,11 +17,10 @@ form.addEventListener('submit', async (event) => {
   console.log(response);
 
   /*
-  some sample code
-    const dataObj = response.results[0];
-    const postImg = clone.querySelector('.post__img');
-    postImg.src = dataObj.urls.small;
-    postImg.alt = dataObj.alt_description;
+  const dataObj = response.results[0];
+  const postImg = clone.querySelector('.post__img');
+  postImg.src = dataObj.urls.small;
+  postImg.alt = dataObj.alt_description;
   */
 
   /*
